497832f55a3440d7828dbe1423afeb690484bf66
Michael Buluma <buluma@users.noreply.github.com>
2023-05-03 18:52:21 +0300

    Update ansible_role_fix_readability.sh

--- ansible_role_fix_readability.sh
+++ ansible_role_fix_readability.sh
@@ -19,40 +19,19 @@ sedder(){
 }
 
 checker() {
-  for folder in $1 ; do
-    file="${folder}/main.yml"
-    echo "File: $file"
-    if [ -f "${file}" ] ; then
-      if grep -q '])' "${file}" ; then
-        sedder 's/])/] )/g' "${file}"
-        echo "Added a space between ] and ) in ${file}."
-      fi
-      if grep -q ')}' "${file}" ; then
-        sedder 's/)}/) }/g' "${file}"
-        echo "Added a space between ) and } in ${file}."
-      fi
+  file="${1}/main.yml"
+  if [ -f "${file}" ] ; then
+    if grep -q '])' "${file}" ; then
+      sedder 's/])/] )/g' "${file}"
+      echo "Added a space between ] and ) in ${file}."
     fi
-  done
+    if grep -q ')}' "${file}" ; then
+      sedder 's/)}/) }/g' "${file}"
+      echo "Added a space between ) and } in ${file}."
+    fi
+  fi
 }
 
-while getopts 'f:' OPTION; do
-  case "$OPTION" in
-    f)
-      sub_folder="$OPTARG"
-      ;;
-    *)
-      echo "Unknow argument: $0 [-f path]" >&2
-      exit 1
-    ;;
-  esac
-done
-
-shift "$(($OPTIND -1))"
-
-if [ -z "$sub_folder" ]; then
-  sub_folder="."
-fi
-
 for type in defaults vars ; do
-  checker "${sub_folder}/${type}"
+  checker "${type}"
 done
